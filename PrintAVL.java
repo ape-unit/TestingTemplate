19fecbfb9bb41e9144a8286edad9e27a28a6471b
Andreas Paljug <andreaspaljug@gmail.com>
2019-03-03 21:58:31 -0500

    added print AVL

---
+++ PrintAVL.java
@@ -0,0 +1,88 @@
+
+    //method for printing out the trees from stack overflow
+
+    public static <T extends Comparable<? super T>> void printTree(AVL<T>  tree) {
+        AVLNode<T> root = tree.getRoot();
+        int maxLevel = maxLevel(root);
+
+        printNodeInternal(Collections.singletonList(root), 1, maxLevel);
+    }
+
+    private static <T extends Comparable<? super T>> void printNodeInternal(List<AVLNode<T>> nodes, int level, int maxLevel) {
+        if (nodes.isEmpty() || isAllElementsNull(nodes))
+            return;
+
+        int floor = maxLevel - level;
+        int endgeLines = (int) Math.pow(2, (Math.max(floor - 1, 0)));
+        int firstSpaces = (int) Math.pow(2, (floor)) - 1;
+        int betweenSpaces = (int) Math.pow(2, (floor + 1)) - 1;
+
+        printWhitespaces(firstSpaces);
+
+        List<AVLNode<T>> newNodes = new ArrayList<AVLNode<T>>();
+        for (AVLNode<T> node : nodes) {
+            if (node != null) {
+                System.out.print(node.getData());
+                newNodes.add(node.getLeft());
+                newNodes.add(node.getRight());
+            } else {
+                newNodes.add(null);
+                newNodes.add(null);
+                System.out.print(" ");
+            }
+
+            printWhitespaces(betweenSpaces);
+        }
+        System.out.println("");
+
+        for (int i = 1; i <= endgeLines; i++) {
+            for (int j = 0; j < nodes.size(); j++) {
+                printWhitespaces(firstSpaces - i);
+                if (nodes.get(j) == null) {
+                    printWhitespaces(endgeLines + endgeLines + i + 1);
+                    continue;
+                }
+
+                if (nodes.get(j).getLeft() != null) {
+                    System.out.print("/");
+                } else {
+                    printWhitespaces(1);
+                }
+                printWhitespaces(i + i - 1);
+
+                if (nodes.get(j).getRight() != null)
+                    System.out.print("\\");
+                else
+                    printWhitespaces(1);
+
+                printWhitespaces(endgeLines + endgeLines - i);
+            }
+
+            System.out.println("");
+        }
+
+        printNodeInternal(newNodes, level + 1, maxLevel);
+    }
+
+    private static void printWhitespaces(int count) {
+        for (int i = 0; i < count; i++)
+            System.out.print(" ");
+    }
+
+    private static <T extends Comparable<? super T>> int maxLevel(AVLNode<T> node) {
+        if (node == null) {
+            return 0;
+        }
+
+        return Math.max(maxLevel(node.getLeft()), maxLevel(node.getRight())) + 1;
+    }
+
+    private static <T> boolean isAllElementsNull(List<T> list) {
+        for (Object object : list) {
+            if (object != null) {
+                return false;
+            }
+        }
+
+        return true;
+    }
